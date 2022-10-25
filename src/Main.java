f5a68a24c74b6ade5ee843ccb13d3afc5e9f2384
david7688 <113467007+david7688@users.noreply.github.com>
2022-10-25 14:06:14 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import java.net.SocketTimeoutException;
+
+public class Main {
+    public static void main(String[] args)
+    {
+        String[] names = {"Bob", "Carol", "Ted", "Alice"};
+        System.out.println(names.length);
+    }
+}
